ee144987cfbbe83b8daba0a35a7e434326b54180
SUNG IL LEE <sungil.lee90@gmail.com>
2020-08-24 15:39:42 -0500

    added another view page for home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # 주소로 처음 들어가면, HomeControlelr에서 index 액션에 연결해주세
+  get '/' => 'home#index'
+  get '/home' => 'home#hi'
   # get 'articles/new'
   # get 'articles/create' Removed these get (mappings), since we already defined by calling resources method
   get 'welcome/index'
