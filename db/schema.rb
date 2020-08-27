4419c30f0de393945e2231cbbe49f87975000bd8
SUNG IL LEE <sungil.lee90@gmail.com>
2020-08-27 12:09:16 -0500

    added Scaffolding

--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,6 @@
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally edit your database, and then regenerate this schema definition.
+# incrementally modify your database, and then regenerate this schema definition.
 #
 # This file is the source Rails uses to define your schema when running `rails
 # db:schema:load`. When creating a new database, `rails db:schema:load` tends to
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_25_190016) do
+ActiveRecord::Schema.define(version: 2020_08_27_034012) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "crudposts", force: :cascade do |t|
+    t.string "description"
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
