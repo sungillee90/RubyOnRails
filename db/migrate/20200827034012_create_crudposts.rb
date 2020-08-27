fe0d5c35d956729d3ed6bc78b925999928d57706
SUNG IL LEE <sungil.lee90@gmail.com>
2020-08-26 22:42:20 -0500

    redid for db migrate

--- db/migrate/20200827034012_create_crudposts.rb
+++ db/migrate/20200827034012_create_crudposts.rb
@@ -1,7 +1,7 @@
 class CreateCrudposts < ActiveRecord::Migration[6.0]
   def change
     create_table :crudposts do |t|
-      t.string, :description
+      t.string :description
       t.string :title
 
       t.timestamps
