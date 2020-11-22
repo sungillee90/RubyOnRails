a64dff13dd06473c94dcb187505a025e09929b52
SUNG IL LEE <sungil.lee90@gmail.com>
2020-11-22 00:16:21 -0600

    practice even number

--- app/practice.rb
+++ app/practice.rb
@@ -1,12 +1,28 @@
 
-$hello = "world"
-
-def call_hello
-  puts $hello
-end
+# $hello = "world"
+#
+# def call_hello
+#   puts $hello
+# end
 
 # # Enter your code here. Read input from STDIN. Print output to STDOUT
 # print "Hello HackerRank!!"
 
 # Enter your code here. Read input from STDIN. Print output to STDOUT
-print self
+# print self
+
+# def odd_or_even(number)
+#
+#   # add your code here
+#   return number.even?
+#
+# end
+#
+# (0...gets.to_i).each do |i|
+#   puts odd_or_even(gets.to_i)
+# end
+
+num = 2
+if num%2 == 0
+  puts "even number"
+end
